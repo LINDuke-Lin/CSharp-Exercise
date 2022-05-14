a44f28eebd8d9cefccfee7128baef6325ba31307
Duke <dukelin@va7.com.tw>
2022-05-14 16:55:54 +0800

    feat: 委派練習1 - 馬戲團

---
+++ CSharpExercise/EventDemo/Messenger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpExercise.PrintHello
+{
+    public class Messenger
+    {
+        /// <summary>
+        /// 主程式
+        /// </summary>
+        public void Main()
+        {
+
+        }
+
+        public delegate void BoilerLogHandler(string status);
+    }
+}
